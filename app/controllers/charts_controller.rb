496f822cdea05f150db0b533091502856659ef4a
bwalshbmw <bmw@useed.org>
2019-02-13 00:13:43 -0500

    Update chart controller and associated tests
    Removes the hard coded data from the previous feature where we setup the chart. This sets up the ability to contact the service that will be used to contact the API and get the up to date exchange rate data. Also, updates the tests to more accurately depict what the controller is responsible for.

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,44 +1,13 @@
 # Controller for managing the data passed to the historic rates chart.
 class ChartsController < ApplicationController
   def historic_rates
-    render json: series_data.chart_json
+    @series_data = RateGetterService.get_rates(requested_currencies)
+    render json: @series_data.chart_json
   end
 
   private
 
-  # Placeholder data used to set up the initial chart. Will be replaced with
-  # data from the API. -BW 02/08/2019
-  def series_data
-    [euro_data, usd_data, aud_data]
-  end
-
-  def euro_data
-    { name: 'EUR',
-      data: {
-        '2018-01-02': 0.2531389226,
-        '2018-01-03': 0.2548679784,
-        '2018-01-04': 0.256016385,
-        '2018-01-05': 0.2560360499
-      } }
-  end
-
-  def usd_data
-    { name: 'USD',
-      data: {
-        '2018-01-02': 0.3054121102,
-        '2018-01-03': 0.3064277704,
-        '2018-01-04': 0.3088837686,
-        '2018-01-05': 0.3083954221
-      } }
-  end
-
-  def aud_data
-    { name: 'AUD',
-      data: {
-        '2018-01-02': 0.3901630215,
-        '2018-01-03': 0.390941992,
-        '2018-01-04': 0.3942140297,
-        '2018-01-05': 0.3932969762
-      } }
+  def requested_currencies
+    'EUR,USD,AUD'
   end
 end
--- spec/controllers/charts_controller_spec.rb
+++ spec/controllers/charts_controller_spec.rb
@@ -2,8 +2,15 @@ require 'rails_helper'
 
 RSpec.describe ChartsController, type: :controller do
   describe 'GET historic rates' do
-    it 'should get the historic rates for the chart' do
-      get :historic_rates, format: :json
+    it 'should call the RateGetterService' do
+      sample_data = { name: 'USD', data: { '2019-01-01': 0.3900000 } }
+      expect(RateGetterService)
+        .to receive(:get_rates).with(any_args) { sample_data }
+      get :historic_rates
+    end
+
+    it 'should send the rates to the chart without error' do
+      get :historic_rates
       expect(response).to have_http_status(:ok)
     end
   end
