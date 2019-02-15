c5f0dda4160f8ff2141ced204dcba75f132a7cec
bwalshbmw <bmw@useed.org>
2019-02-15 15:56:09 -0500

    Update controller to set session currency
    Updates the ExchangeRatesController so that it takes the currency provided in the params and adds it to the session data. This allows the currency value to be accessible to the ChartsController which needs it to send an updated API request to get the specified currency data.

--- spec/controllers/exchange_rates_controller_spec.rb
+++ spec/controllers/exchange_rates_controller_spec.rb
@@ -6,5 +6,15 @@ RSpec.describe ExchangeRatesController, type: :controller do
       get :index
       expect(response).to have_http_status(:ok)
     end
+
+    it 'should set the currency for the session when the param is provided' do
+      get :index, params: { currency: 'USD' }
+      expect(session[:currency]).to eq 'USD'
+    end
+
+    it 'should not set the currency for the session when there is no param' do
+      get :index
+      expect(session[:currency]).to be_blank
+    end
   end
 end
